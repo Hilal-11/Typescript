1db09f6af1b5b123381c0c47c863a1b705a01621
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-07 22:43:39 +0530

    ts basics

---
+++ basics.js
@@ -0,0 +1,9 @@
+console.log("Hello Typescript");
+var username = "Hilal";
+var user_id = 1818;
+var isMinor = false;
+var notype = 19;
+console.log(username);
+console.log(user_id);
+console.log(isMinor);
+console.log(notype);
---
+++ basics.ts
@@ -0,0 +1,13 @@
+console.log("Hello Typescript")
+
+
+const username: string = "Hilal"
+const user_id: number = 1818
+const isMinor: boolean = false
+
+const notype : any = 19 
+
+console.log(username)
+console.log(user_id)
+console.log(isMinor)
+console.log(notype)
