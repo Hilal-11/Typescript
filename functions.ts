a8e1633efd10c53cbb4a9856c678718157c76c65
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-03-12 17:19:38 +0530

    typescript'

--- functions.ts
+++ functions.ts
@@ -14,36 +14,70 @@
 // let user : string;
 
 
-function greeting(user:string) {
-    return `HELLO ${user.toUpperCase()}`
+// function greeting(user:string) {
+//     return `HELLO ${user.toUpperCase()}`
+// }
+// let getuser = greeting("Hilal");
+// console.log(getuser)
+
+
+
+
+// //  function type (i mean what value function could return in TS)
+
+// function getFavoriteNumber ():number {
+//     return Math.floor(Math.random() * 100)
+// }
+
+// let fav_num = getFavoriteNumber();
+// console.log(fav_num)
+
+
+
+
+// const names:string[] = ["Alice" , "Bob" , "Eva"];
+
+// names.forEach(function (name) {
+//     console.log(name.replace(name , "Hilal").toUpperCase())
+// })
+
+// names.forEach((name) => {
+//     console.log(name.toUpperCase())
+// })
+
+//  types of functions in typescript
+
+function greeting(username : string) {
+    return username.toUpperCase()
 }
-let getuser = greeting("Hilal");
-console.log(getuser)
+const username: string = "Hilal"
+greeting(username);
 
 
 
-
-//  function type (i mean what value function could return in TS)
-
-function getFavoriteNumber ():number {
+function favNumber() : number{
     return Math.floor(Math.random() * 100)
 }
 
-let fav_num = getFavoriteNumber();
-console.log(fav_num)
+const favNum = favNumber();
+console.log(favNum)
+
+// type is (Any) --> so it is problematic
+// function add(num) {
+//     return num + 10;
+// }
+// add(6)
+
+// function add(num: number) {
+//     return num + 10;
+// }
+
+// add(6)
 
 
 
 
-const names:string[] = ["Alice" , "Bob" , "Eva"];
 
-names.forEach(function (name) {
-    console.log(name.replace(name , "Hilal").toUpperCase())
-})
-
-names.forEach((name) => {
-    console.log(name.toUpperCase())
-})
 
 
 
